0c0dbc63bca9d1338581c10df19ad1be9d4a1eac
bower-chosen <notmyemail@bower-chosen.lol>
2017-11-06 17:24:02 +0200

    changed package name

---
+++ src/main/java/org/mtudor/maven/plugins/echo/EchoMojo.java
@@ -0,0 +1,51 @@
+package org.mtudor.maven.plugins.echo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
+
+@Mojo( name = "echo")
+public class EchoMojo extends AbstractMojo
+{
+    @Parameter(property = "echo.expressions")
+    private List<String> echoExpressions;
+
+    @Parameter(property = "echo.expression")
+    private String echoExpression;
+
+    public void execute() throws MojoExecutionException {
+        printExpression();
+        printExpressions();
+    }
+
+    public void printExpression() throws MojoExecutionException {
+        String lineSep = System.getProperty("line.separator");
+        if (echoExpression != null){
+            StringBuilder out = new StringBuilder(lineSep);
+            final String[] split = echoExpression.split(";");
+            for (int i = 0; i < split.length ; i++){
+                out.append(split[i]);
+                if (i != split.length-1){
+                    out.append(lineSep);
+                }
+            }
+            getLog().info(out.toString());
+        }
+    }
+    public void printExpressions() throws MojoExecutionException {
+        String lineSep = System.getProperty("line.separator");
+        if (echoExpressions != null){
+            StringBuilder out = new StringBuilder(lineSep);
+            for (int i = 0; i < echoExpressions.size() ; i++){
+                out.append(echoExpressions.get(i));
+                if (i != echoExpressions.size()-1){
+                    out.append(lineSep);
+                }
+            }
+            getLog().info(out.toString());
+        }
+    }
+}
